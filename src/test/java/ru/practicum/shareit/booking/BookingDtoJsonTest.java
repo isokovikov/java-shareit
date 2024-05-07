3e54e3c2c62e1111912839eb6818ed9fd1ca213c
isokovikov <i.sokovikov@gs1ru.org>
2024-05-07 20:56:06 +0300

    fix: time formatter

--- src/test/java/ru/practicum/shareit/booking/BookingDtoJsonTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingDtoJsonTest.java
@@ -53,7 +53,7 @@ class BookingDtoJsonTest {
     @Test
     void testSerialize() throws Exception {
         JsonContent<BookingDto> result = json.write(bookingDto);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSS");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSS");
 
         assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(1);
         assertThat(result).extractingJsonPathStringValue("$.start").isEqualTo(bookingDto.getStart().format(formatter));
