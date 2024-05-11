9e4fff3bd882ec960e3ab1574076f45336fb6807
isokovikov <i.sokovikov@gs1ru.org>
2024-05-11 11:15:47 +0300

    fix: ТЗ-16

--- server/src/main/java/ru/practicum/server/item/service/ItemServiceImpl.java
+++ server/src/main/java/ru/practicum/server/item/service/ItemServiceImpl.java
@@ -26,7 +26,6 @@ import ru.practicum.server.user.repository.UserRepository;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
